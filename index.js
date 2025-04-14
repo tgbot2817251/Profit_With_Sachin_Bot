e4b7ec641a7b937220c5aaacc3d24e57d5b2d23a
EarningHelper <team19sachin@gmail.com>
2025-04-14 09:17:17 +0530

    Updated feature

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 // 🔹 Aapke Telegram Channels (Channel IDs)
 const channel1 = '-1002494596204'; // desi earning
 const channel2 = '-1002262588651'; // prediction tool
+const channel3 = '-1002382031113'; // wingo prediction
 
 // 🔹 Start Command
 bot.start(async (ctx) => {
@@ -20,6 +21,9 @@ bot.start(async (ctx) => {
                     [
                         { text: "📢 𝗝𝗢𝗜𝗡 𝗖𝗛𝗔𝗡𝗡𝗘𝗟 𝟭", url: "https://t.me/+KsAa0h2iRmw3YjI1" },
                         { text: "📢 𝗝𝗢𝗜𝗡 𝗖𝗛𝗔𝗡𝗡𝗘𝗟 𝟮", url: "https://t.me/+gpt7bMQia4kyMzJl" }
+                    ],
+                    [
+                        { text: "📢 𝗝𝗢𝗜𝗡 𝗖𝗛𝗔𝗡𝗡𝗘𝗟 𝟯", url: "https://t.me/+6PJfm3k9NThmOThl" }
                     ],
 
                     [
@@ -42,7 +46,10 @@ bot.action('verify', async (ctx) => {
         const res2 = await ctx.telegram.getChatMember(channel2, userId);
         const isMember2 = ['member', 'administrator', 'creator', 'restricted'].includes(res2.status);
 
-        if (isMember1 && isMember2) {
+        const res3 = await ctx.telegram.getChatMember(channel2, userId);
+        const isMember3 = ['member', 'administrator', 'creator', 'restricted'].includes(res3.status);
+
+        if (isMember1 && isMember2 && isMember3) {
             await ctx.replyWithPhoto(
                 'https://t.me/Only_4_photos/38',
                 {
